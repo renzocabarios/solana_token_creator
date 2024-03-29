52571a2e9b8fb2a3e4b143963a09c09a5c1841be
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-29 20:42:18 +0800

    Add CreateNftForm

---
+++ app/nft/(form)/page.tsx
@@ -0,0 +1,139 @@
+"use client";
+import { useState } from "react";
+import useUmi from "../../useUmi";
+import {
+  generateSigner,
+  percentAmount,
+  publicKey,
+  sol,
+  transactionBuilder,
+} from "@metaplex-foundation/umi";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { createNft } from "@metaplex-foundation/mpl-token-metadata";
+import { transferSol } from "@metaplex-foundation/mpl-toolbox";
+import { SOLANA_CONFIG } from "@/env";
+import axios from "axios";
+import { Button, ImageInput, InputField } from "@/components";
+
+export default function CreateNftForm() {
+  const umi = useUmi();
+  const wallet = useWallet();
+  const [form, setform] = useState({
+    name: "",
+    description: "",
+    symbol: "",
+    sellerFeeBasisPoints: 0,
+  });
+
+  const [current, setcurrent] = useState<null | any>(null);
+
+  const onInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setform((state) => ({ ...state, [name]: value }));
+  };
+
+  const onFileChange = async (e: any) => {
+    setcurrent(e.target.files[0]);
+  };
+
+  const uploadOffChain = async () => {
+    const request = new FormData();
+    request.append("image", current);
+    request.append(
+      "metadata",
+      JSON.stringify({
+        name: form.name,
+        symbol: form.symbol,
+        description: form.description,
+      })
+    );
+    const {
+      data: { costs, uri },
+    } = await axios.post("api/irys", request);
+
+    return { costs, uri };
+  };
+
+  // TODO: Change the approach when caluclating/getting uploading costs
+  const onCreateMint = async () => {
+    const { costs, uri } = await uploadOffChain();
+
+    const mint = generateSigner(umi);
+
+    const mintInstruction = await createNft(umi, {
+      mint,
+      name: form.name,
+      symbol: form.symbol,
+      uri,
+      sellerFeeBasisPoints: percentAmount(form.sellerFeeBasisPoints),
+    });
+
+    const transferInstruction = transferSol(umi, {
+      source: umi.identity,
+      destination: publicKey(SOLANA_CONFIG.treasury),
+      amount: sol(0.001 + costs + costs),
+    });
+
+    const builder = transactionBuilder()
+      .add(mintInstruction)
+      .add(transferInstruction);
+
+    await builder.sendAndConfirm(umi);
+  };
+
+  const parseFileToString = () => {
+    let image: null | string;
+    try {
+      image = URL.createObjectURL(current);
+    } catch (_) {
+      image = null;
+    }
+    return image;
+  };
+
+  return (
+    <div className="flex flex-col gap-5 ">
+      <div className="flex gap-10">
+        <div className="col-span-2">
+          <ImageInput
+            image={parseFileToString()}
+            onChange={onFileChange}
+            label="name"
+            name="name"
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-4 p-2">
+          <InputField
+            type={"text"}
+            onChange={onInputChange}
+            label="name"
+            name="name"
+          />
+          <InputField
+            type={"text"}
+            onChange={onInputChange}
+            label="symbol"
+            name="symbol"
+          />
+          <InputField
+            type={"text"}
+            onChange={onInputChange}
+            label="description"
+            name="description"
+          />
+          <InputField
+            type={"number"}
+            onChange={onInputChange}
+            label="sellerFeeBasisPoints"
+            name="sellerFeeBasisPoints"
+          />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        {wallet.connected && (
+          <Button onClick={onCreateMint} label="Create Mint" />
+        )}
+      </div>
+    </div>
+  );
+}
--- app/nft/page.tsx
+++ app/nft/page.tsx
@@ -1,20 +1,6 @@
-"use client";
-import { useState } from "react";
-import useUmi from "../useUmi";
-import {
-  generateSigner,
-  percentAmount,
-  publicKey,
-  sol,
-  transactionBuilder,
-} from "@metaplex-foundation/umi";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { createNft } from "@metaplex-foundation/mpl-token-metadata";
-import { transferSol } from "@metaplex-foundation/mpl-toolbox";
-import { SOLANA_CONFIG } from "@/env";
-import axios from "axios";
-import { Button, ImageInput, InputField, Navbar } from "@/components";
 import Link from "next/link";
+import { Navbar } from "@/components/navbar";
+import CreateNftForm from "./(form)/page";
 
 export default function Home() {
   return (
@@ -26,130 +12,9 @@ export default function Home() {
           <Link href={"/nft"}>Mint NFT</Link>
         </div>
         <div className="flex justify-center items-center h-[90vh] w-[85vw] gap-10 ">
-          <CreateMintForm />
+          <CreateNftForm />
         </div>
       </div>
     </main>
-  );
-}
-
-function CreateMintForm() {
-  const umi = useUmi();
-  const wallet = useWallet();
-  const [form, setform] = useState({
-    name: "",
-    description: "",
-    symbol: "",
-    sellerFeeBasisPoints: 0,
-  });
-
-  const [current, setcurrent] = useState<null | any>(null);
-
-  const onInputChange = (e: any) => {
-    const { name, value } = e.target;
-    setform((state) => ({ ...state, [name]: value }));
-  };
-
-  const onFileChange = async (e: any) => {
-    setcurrent(e.target.files[0]);
-  };
-
-  const uploadOffChain = async () => {
-    const request = new FormData();
-    request.append("image", current);
-    request.append(
-      "metadata",
-      JSON.stringify({
-        name: form.name,
-        symbol: form.symbol,
-        description: form.description,
-      })
-    );
-    const {
-      data: { costs, uri },
-    } = await axios.post("api/irys", request);
-
-    return { costs, uri };
-  };
-
-  // TODO: Change the approach when caluclating/getting uploading costs
-  const onCreateMint = async () => {
-    const { costs, uri } = await uploadOffChain();
-
-    const mint = generateSigner(umi);
-
-    const mintInstruction = await createNft(umi, {
-      mint,
-      name: form.name,
-      symbol: form.symbol,
-      uri,
-      sellerFeeBasisPoints: percentAmount(form.sellerFeeBasisPoints),
-    });
-
-    const transferInstruction = transferSol(umi, {
-      source: umi.identity,
-      destination: publicKey(SOLANA_CONFIG.treasury),
-      amount: sol(0.001 + costs + costs),
-    });
-
-    const builder = transactionBuilder()
-      .add(mintInstruction)
-      .add(transferInstruction);
-
-    await builder.sendAndConfirm(umi);
-  };
-
-  const parseFileToString = (file: Blob | MediaSource) => {
-    let image: null | string;
-    try {
-      image = URL.createObjectURL(current);
-    } catch (_) {
-      image = null;
-    }
-    return image;
-  };
-
-  return (
-    <div className="grid grid-cols-2 gap-4 p-7 rounded-lg ">
-      <div className="col-span-2">
-        <ImageInput
-          image={parseFileToString(current)}
-          onChange={onFileChange}
-          label="name"
-          name="name"
-        />
-      </div>
-
-      <InputField
-        type={"text"}
-        onChange={onInputChange}
-        label="name"
-        name="name"
-      />
-      <InputField
-        type={"text"}
-        onChange={onInputChange}
-        label="symbol"
-        name="symbol"
-      />
-      <InputField
-        type={"text"}
-        onChange={onInputChange}
-        label="description"
-        name="description"
-      />
-      <InputField
-        type={"number"}
-        onChange={onInputChange}
-        label="sellerFeeBasisPoints"
-        name="sellerFeeBasisPoints"
-      />
-
-      {wallet.connected && (
-        <div className="col-span-2">
-          <Button onClick={onCreateMint} label="Create Mint" />
-        </div>
-      )}
-    </div>
   );
 }
