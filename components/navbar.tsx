3f689f50bd7c90d62bce472555c7d311b242b933
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-29 20:42:10 +0800

    Add navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useEffect, useState } from "react";
+
+export function Navbar() {
+  const [mounted, setmounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setmounted(true);
+  }, [setmounted]);
+
+  return (
+    <div className="w-full bg-slate-900 h-[10vh] flex justify-between items-center px-10">
+      <p className="text-xl font-semibold">Solana Meme Coin Generator</p>
+      {mounted && <WalletMultiButton />}
+    </div>
+  );
+}
