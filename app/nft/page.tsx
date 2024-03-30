995f450706e4e9f3498f3a082e19c6a66afca4af
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-30 21:33:48 +0800

    Update CreateNFT

--- app/nft/page.tsx
+++ app/nft/page.tsx
@@ -1,20 +1,9 @@
-import Link from "next/link";
-import { Navbar } from "@/components/navbar";
 import CreateNftForm from "./(form)/page";
 
-export default function Home() {
+export default function CreateNFT() {
   return (
-    <main className="min-h-screen flex flex-col">
-      <Navbar />
-      <div className="flex">
-        <div className="h-[90vh] w-[15vw] flex flex-col p-3 gap-3 bg-slate-900">
-          <Link href={"/"}>Mint Tokens</Link>
-          <Link href={"/nft"}>Mint NFT</Link>
-        </div>
-        <div className="flex justify-center items-center h-[90vh] w-[85vw] gap-10 ">
-          <CreateNftForm />
-        </div>
-      </div>
-    </main>
+    <>
+      <CreateNftForm />
+    </>
   );
 }
