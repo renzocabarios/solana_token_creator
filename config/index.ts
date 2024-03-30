77804f7b815cf7e2e844ba0846eceab9c9b718c1
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-30 21:33:15 +0800

    Update env

--- config/index.ts
+++
@@ -1,3 +0,0 @@
-export const SOLANA = {
-  rpc: process.env.NEXT_PUBLIC_RPC_URL ?? "https://api.devnet.solana.com",
-};
--- env/index.ts
+++ env/index.ts
@@ -1,5 +1,10 @@
+import { Connection } from "@solana/web3.js";
+
 export const SOLANA_CONFIG = {
   rpc: process.env.NEXT_PUBLIC_RPC_URL || "https://api.devnet.solana.com",
+  connection: new Connection(
+    process.env.NEXT_PUBLIC_RPC_URL ?? "https://api.devnet.solana.com"
+  ),
   payer_private_key: process.env.NEXT_PUBLIC_PAYER_PRIVATE_KEY || "",
   treasury: process.env.NEXT_PUBLIC_TREASURY || "",
 };
