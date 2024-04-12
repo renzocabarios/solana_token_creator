c75a7e40d907f0afc11acc7a5a7f5bec4a50ea45
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-12 08:59:25 +0800

    Update CreateTokenStore

--- lib/zustand/create-token.store.ts
+++ lib/zustand/create-token.store.ts
@@ -1,12 +1,15 @@
 import { createSelectors } from "@/lib/zustand/createSelectors";
 import { create } from "zustand";
+import { MetadataSchema, metadataDefaults } from "../schemas/metadata.schema";
 
 interface CreateTokenState {
   page: number;
+  metadata: MetadataSchema;
 }
 
 interface CreateTokenActions {
   setPage: (page: number) => void;
+  setMetadata: (data: MetadataSchema) => void;
   handleNextPage: () => void;
   handleBackPage: () => void;
 }
@@ -15,10 +18,12 @@ type CreateTokenStore = CreateTokenState & CreateTokenActions;
 
 const initialState: CreateTokenState = {
   page: 0,
+  metadata: metadataDefaults,
 };
 
 const useCreateTokenStoreBase = create<CreateTokenStore>((set) => ({
   setPage: (page: number) => set(() => ({ page })),
+  setMetadata: (data) => set(() => ({ metadata: data })),
   handleNextPage: () => set((state) => ({ page: state.page + 1 })),
   handleBackPage: () => set((state) => ({ page: state.page - 1 })),
   ...initialState,
