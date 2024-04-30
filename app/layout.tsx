573ef3f0bde8ddb193a17ff79a2166d47676c98b
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-30 16:11:24 +0800

    Add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,13 @@
-"use client";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { useEffect, useState } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { SOLANA_CONFIG } from "@/env";
-import Warning from "./(components)/warning";
+
+import Root from "./(components)/root";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [mounted, setmounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    setmounted(true);
-  }, [setmounted]);
-
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ConnectionProvider endpoint={SOLANA_CONFIG.rpc}>
-          <WalletProvider wallets={[]} autoConnect>
-            <WalletModalProvider>
-              <main className="bg-slate-800 min-h-screen">
-                <Warning />
-                <div className="min-h-[95vh]">{mounted && children}</div>
-              </main>
-            </WalletModalProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </body>
-    </html>
-  );
+  return <Root>{children}</Root>;
 }
